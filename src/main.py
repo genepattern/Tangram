8ed8fbc38dbaf5c6b0426c3cf68cbaa83f573edc
Edwin Huang <43209173+edwin5588@users.noreply.github.com>
2024-02-16 13:40:07 -0800

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,2 +1,14 @@
 ## python script to run main function 
 ## Add any argument parsing, function calling, here
+
+import argparse
+from functions import * 
+
+
+
+
+## Parsing from command line, and running the script. 
+if __name__ == "__main__":
+
+  parser = argparse.ArgumentParser()
+  args = parser.parse_args()
