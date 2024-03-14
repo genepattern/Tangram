79670cb8d0d85d99535a4ab6fd8f4d5c9402b8d5
Omar Halawa <113780419+omarhalawa3301@users.noreply.github.com>
2024-03-14 14:27:57 +0300

    Adding plot_auc_replacement()

--- src/functions.py
+++ src/functions.py
@@ -1,11 +1,43 @@
 ## python script to include required functions for running
 ## add any required functions here
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import tangram as tg
 
+def plot_auc_replacement(df_all_genes, test_genes=None):
+    """
+        A replacement function of the tangram-sc library's plot_auc.
+        Plots auc curve which is used to evaluate model performance.
+    
+    Args:
+        df_all_genes (Pandas dataframe): returned by compare_spatial_geneexp(adata_ge, adata_sp); 
+        test_genes (list): list of test genes, if not given, test_genes will be set to genes where 'is_training' field is False
 
-def plot_auc_replacement():
-  ## add any parameters as necessary
-  ## replaces tangram scatterplot function
-  return None
+    Returns:
+        None
+    """
+    metric_dict, ((pol_xs, pol_ys), (xs, ys)) = tg.eval_metric(df_all_genes, test_genes)
+    
+    fig = plt.figure()
+    plt.figure(figsize=(6, 5))
+
+    plt.plot(pol_xs, pol_ys, c='r')
+    sns.scatterplot(data=pd.DataFrame({'x': xs, 'y': ys}), alpha=0.5)
+        
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.0])
+    plt.gca().set_aspect(.5)
+    plt.xlabel('score')
+    plt.ylabel('spatial sparsity')
+    plt.tick_params(axis='both', labelsize=8)
+    plt.title('Prediction on test transcriptome')
+    
+    textstr = 'auc_score={}'.format(np.round(metric_dict['auc_score'], 3))
+    props = dict(boxstyle='round', facecolor='wheat', alpha=0.3)
+    # place a text box in upper left in axes coords
+    plt.text(0.03, 0.1, textstr, fontsize=11, verticalalignment='top', bbox=props);
+
 
 def read_data():
   ## add any parameters as necessary
