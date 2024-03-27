768641868fa9e59a89545c871ab54eca66d23f0f
Omar Halawa <113780419+omarhalawa3301@users.noreply.github.com>
2024-03-27 00:47:40 -0700

    Pre-meeting argparse update
    Adding all params from both notebooks (w and w/o squidpy)

--- src/main.py
+++ src/main.py
@@ -9,6 +9,7 @@ from functions import *
 def main():
 
   # File input arguments
+
   # Spatial data file input (.gct):
   parser.add_argument("--sp", "--spatial", help="spatial data file input"
                       + " Valid file format(s): .gct")
@@ -16,44 +17,68 @@ def main():
   parser.add_argument("--sc", "--scrna", help="single-cell data file input"
                       + " Valid file format(s): .gct")
 
-  # TODO: remove nargs and const, keeping them now for likely smoother debugging - feel free to change
-  # Tangram parameter arguments
-  # Alignment mode argument, accepted values are either "cluster", "cell", or "constrained"; gpu pref if "cell":
-  # TODOC: see notebook for more details on these to have a concise parameter description in module
-  parser.add_argument("-m", "--mode", help="tangram alignment mode (cluster, cell, or constrained)",
-                      nargs="?", const=1, default="cluster", choices=["cluster","cell","constrained"])
 
-  # TODO: add for training data what is likely three argparse arguments: one as a bool argument for whether or not to use
-  # the top n differentially expressed genes, one as the n-value for top differentially expressed genes to use, and an argument
-  # in case the user has a specific set of genes they want to use as training data and opted for F in the first arg (investigate data type for this)
+  # Tangram parameter arguments
+  # TODO: remove nargs and const, keeping them now for likely smoother debugging - feel free to change  
+
+  # Classification mode ("test-train" or "loocv"):
+  parser.add_argument("-m", "--classification_mode", help="tangram classification mode (test-train or loocv)",
+                      nargs="?", const=1, default="test-train", choices=["test-train","loocv"])
+  
+  # UMAP point size argument, used for one plot (initial umap); integer value:
+  parser.add_argument("--umap_point_size", help="umap point size in single-cell initial scanpy plot",
+                    nargs="?", const=1, type=int, default=10)
+  
+  # Alpha value for scanpy spatial plot opacity; float value ranging from 0.0 to 1.0, inclusive:
+  parser.add_argument("--spatial_alpha", help="alpha value in single-cell scanpy spatial plots",
+                    nargs="?", const=1, type=float, default=0.7)
+  
+  # Alpha value for training score plot opacity; float value ranging from 0.0 to 1.0, inclusive:
+  parser.add_argument("--training_alpha", help="alpha value in training score plots",
+                    nargs="?", const=1, type=float, default=0.5)
+
+  # Boolean value for whether to use top n differentially expressed genes as training genes:
+  parser.add_argument("--use_top_n", help="boolean for whether to use top n differentially expressed" 
+                    + "genes for training", nargs="?", const=1, type=bool, default=True)
+  
+  # Number of top differentially expressed genes to use as training genes, only considered if use_top_n is true:
+  parser.add_argument("-n", "--number_training_genes", help="number of top differentially expressed genes to"
+                    + "use for training", nargs="?", const=1, type=int, default=100)
+  
+  # File input for manual training marker gene selection, only considered if use_top_n is false:
+  # TODO: decide on necessity of parameter and file format (simple tsv of names?)
+  parser.add_argument("--marker_genes_input", help="file input in case of manual training marker gene selection",
+                    nargs="?", const=1)
+
+  # Alignment mode argument, accepted values are either "cluster", "cell", or "constrained"; gpu pref if "cell":
+  parser.add_argument("--alignment_mode", help="tangram alignment mode (cluster, cell, or constrained)",
+                      nargs="?", const=1, default="cluster", choices=["cluster","cell","constrained"])
  
   # Alignment cell density argument, accepted values are either "rna_count_based" (cell density prop. to number of RNA molecules) 
   # or "uniform" (if spatial voxels at single cell resolution):
-  # TODO: investigate the np.array passed-in usage of this in the with-squidpy notebook (deconvolving part), likely not a MVP issue though
   parser.add_argument("--density_prior", help="tangram alignment cell density within each spatial voxel (uniform or rna_count_based)",
                       nargs="?", const=1, default="rna_count_based", choices=["rna_count_based","uniform"])
   
   # Color-mapping percent for plots argument, accepted values are floats between 0 and 1:
-  # REF: change name of this param to something better? Essentially a substitute for the "perc" args set to 0.02 in the notebooks
   # TODO: look into how to potentially obtain a somewhat optimal value of this for users, might be useful to investigate
   # some of the commented-out "robust" argument code in tg; could potentially calculate this value for users based on outliers
-  parser.add_argument("--plot_percent", help="tangram range of color-mapping for plots (adjust to remove outliers)",
-                      nargs="?", const=1, default="cluster", type=float)
+  parser.add_argument("--perc", help="tangram range of color-mapping for plots (adjust to remove outliers)",
+                      nargs="?", const=1, default=0.02, type=float)
 
-  # TODO: add an argument for a list of genes to plot individually using plot_genes_sc(), see cell 15-17 in the with-squidpy notebook
-  # Unsure of whether to use string input param or file input - need to investigate
+  # File input of genes to plot measured vs predicted for:
+  # TODO: decide on file format (simple tsv of names?)
+  parser.add_argument("--genes_to_plot", help="file input of genes to plot measured vs predicted for",
+                    nargs="?", const=1)
 
-  
+
   # Developer & verbosity arguments
-  # RFE: keep as ints (0/1 for F/T to be handled by module dropdown as "True"/"False") or other method? 
-  # Module verbosity argument, either True or False, False by default
+  # Module verbosity argument, either True (1) or False (0), False by default
   parser.add_argument("-v", "--verbose", help="module verbosity flag",
-                      nargs="?", const=1, default=False, type=int)
-  # Module debug argument, either True or False, False by default
+                      nargs="?", const=1, default=0, type=int)
+  # Module debug argument, either True (1) or False (0), False by default
   parser.add_argument("-d", "--debug", help="module debug flag",
-                      nargs="?", const=1, default=False, type=int)
+                      nargs="?", const=1, default=0, type=int)
   
-  ## add any parameters as necessary
   return None
 
 
