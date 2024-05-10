a5243107746abec403e1e98ca8c7484551335e85
Omar Halawa <113780419+omarhalawa3301@users.noreply.github.com>
2024-05-10 13:56:17 -0700

    cv methods update

--- src/functions.py
+++ src/functions.py
@@ -22,15 +22,15 @@ def h5ad_input(input):
     adata = sc.read_h5ad(input)
     return adata
 
-def plot_initial(adata_sc, adata_sp, sp_cluster_field, sc_celltype_field, sc_alpha, umap_dotsize, sc_filename, umap_filename):
+def plot_initial(adata_sc, adata_sp, sc_celltype_field, sp_cluster_field, sc_alpha, umap_dotsize, sc_filename, umap_filename):
     """
     Outputs initial plots of sc/snrna and spatial AnnData objects to files.
     
     Args:
         adata_sc (AnnData): object of single-cell data
         adata_sp (AnnData): object of spatial data
-        sp_cluster_field (str): name of .obs field in spatial data with cluster groupings 
         sc_celltype_field (str): name of .obs field in sc/snrna data with cell-type groupings
+        sp_cluster_field (str): name of .obs field in spatial data with cluster groupings 
         sc_alpha (float): opacity value for single-cell data plot (lower is more transparent)
         umap_dotsize (int): umap dot size for spatial data plot
         sc_filename (str): output filename for single-cell data plot
@@ -70,10 +70,11 @@ def gmt_input(filename):
 
     return genes
 
+# TODO: Clarify in module and documentation the usage for cv and tt modes
 def read_markers(adata_sc, mode, sc_celltype_field, n, gmt_file):
     """
-    Only utilized in test-train prediction mode.
-    Evaluates training mode (use top n genes across cell types, .gmt file input, or all genes shared between the datasets) and returns training genes.
+    Only utilized in BOTH test-train and cv prediction modes.
+    Evaluates training mode (use top n genes across cell types, .gmt file input, or all genes shared between the datasets) and returns training genes (all genes to be used in case of cv).
         
     Args:
         adata_sc (AnnData): object of single-cell data
@@ -111,6 +112,40 @@ def preprocessing(adata_sc, adata_sp, markers):
     """
 
     tg.pp_adatas(adata_sc, adata_sp, genes=markers)
+
+def cv(adata_sc, adata_sp, mode, cv_mode, sc_celltype_field, device, num_epochs, verbose):
+    """
+    Performs cross-validation on the data.
+    
+    Args:
+        adata_sc (AnnData): object of single cell data
+        adata_sp (AnnData): object of spatial data
+        mode (str): choice of either one of:
+                "cells" (default, GPU recommended), 
+                "clusters" (averages single cells belonging to same cluster),
+                "constrained" (for deconvolution)
+        cv_mode (str): either one of "loo" (leave-one-out) or "10fold"
+        sc_celltype_field (str): for "constrained" mode, name of .obs field in sc/snrna data with cell-type groupings
+        device (str): "cuda:0" (GPU) or "cpu"
+        num_epochs (int): # of iterations for function mapping
+
+    Returns:
+        ad_ge_cv (AnnData): object of new spatial mapping
+        df_test_genes (DataFrame): matrix of test genes' scores and sparsities 
+    """
+
+    cv_dict, ad_ge_cv, df_test_genes = tg.cross_val(adata_sc, adata_sp, 
+                                     device=device, 
+                                     mode=mode,
+                                     cv_mode=cv_mode,
+                                     num_epochs=num_epochs,
+                                     cluster_label=sc_celltype_field,
+                                     return_gene_pred=True,
+                                     verbose=verbose)
+    
+    print("Average cross-validation scores:", cv_dict)
+
+    return ad_ge_cv, df_test_genes
 
 def find_alignment(adata_sc, adata_sp, mode, sc_celltype_field, density_prior, num_epochs, device):
     """
@@ -174,7 +209,7 @@ def map_scrna_to_space(adata_map, adata_sc, adata_sp, sc_celltype_field, celltyp
         celltype_map_filename (str): Filename to output the plot
         
     Returns:
-        Plots with spatial mapping
+        N/A
     """
 
     tg.project_cell_annotations(adata_map, adata_sp, annotation=sc_celltype_field)
@@ -183,8 +218,6 @@ def map_scrna_to_space(adata_map, adata_sc, adata_sp, sc_celltype_field, celltyp
     
     plt.savefig(celltype_map_filename)
     plt.close()
-
-    return annotation_list
 
 # Replacement function of tg.plot_training_scores() for saving output plot
 def plot_training_replacement(adata_map, training_alpha, num_bins, filename):
@@ -299,31 +332,55 @@ def plot_test_replacement(df_gene_score, bins, alpha, filename):
     plt.tight_layout()
     plt.savefig(filename)
 
-# TODO: add function comment
 def project(adata_map, adata_sc, predictions_filename):
+    """
+    Generates "new spatial data" using the now-mapped sc/snrna data.
+        
+    Args:
+        adata_map (AnnData): object of spatial mapping (from find_alignment()) 
+        adata_sc (AnnData): object of single-cell data
+        predictions_filename (str): filename of new spatial data object to output
+
+    Returns:
+        AnnData object of new spatial mapping
+    """
 
     ad_ge = tg.project_genes(adata_map, adata_sc)
     ad_ge.write(predictions_filename)
 
     return ad_ge
 
+def compare_spatial(ad_ge, adata_sp, adata_sc, data_filename):
+    """
+    Compute similarity scores of all genes between measured and predicted values.
+        
+    Args:
+        adata_sp (AnnData): object of original spatial data
+        adata_sc (AnnData): object of single-cell data
+        adata_ge (AnnData): object of new spatial data (from project())
+        data_filename (str): filename of new spatial data object to output
+
+    Returns:
+        DataFrame of all genes' (train and test) similarity scores and sparsity statistics
+    """
+    df_all_genes = tg.compare_spatial_geneexp(ad_ge, adata_sp, adata_sc)
+    df_all_genes.to_csv(data_filename, sep='\t')
+
+    return df_all_genes
+
 # Replacement function of tg.plot_auc() due to seaborn version issues
-def plot_auc_replacement(ad_ge, adata_sp, adata_sc, plot_filename, data_filename, test_genes=None):
+def plot_auc_replacement(df_all_genes, plot_filename, test_genes=None):
     """
       Plots auc curve which is used to evaluate model performance.
     
     Args:
-        ad_ge (AnnData): anndata object of predicted spatial data
-        adata_sp (AnnData): object of input spatial data
-        adata_sc (AnnData): object of input sc/snrna data
+        df_all_genes (DataFrame): matrix of all genes, train and test, and their sparsity statistics (from compare_spatial())
+        plot_filename (str): filename to output auc plot to
         test_genes (list of strs): test genes, and if not given, test_genes will be set to genes where 'is_training' field is False
 
     Returns:
         N/A
     """
-
-    df_all_genes = tg.compare_spatial_geneexp(ad_ge, adata_sp, adata_sc)
-    df_all_genes.to_csv(data_filename, sep='\t')
     
     metric_dict, ((pol_xs, pol_ys), (xs, ys)) = tg.eval_metric(df_all_genes, test_genes)
     
@@ -347,14 +404,27 @@ def plot_auc_replacement(ad_ge, adata_sp, adata_sc, plot_filename, data_filename
     plt.text(0.03, 0.1, textstr, fontsize=11, verticalalignment='top', bbox=props)
     plt.savefig(plot_filename)
 
-# TODO: add function comment
 def plot_genes(genes_filepath, adata_sp, ad_ge, plot_genes_outpath, perc):
-  
-  genes = gmt_input(genes_filepath)
-  fig = tg.plot_genes_sc(genes, adata_measured=adata_sp, adata_predicted=ad_ge, perc=perc, return_figure=True)
+    """
+    Plots measured vs predicted transcriptomic gene profiles in space 
+        
+    Args:
+        genes_filepath (str): filename of one-line GMT file to take in genes to plot
+        adata_sp (AnnData): object of original spatial data
+        adata_ge (AnnData): object of new spatial data (from project())
+        plot_genes_outpath (str): filename to output gene predicted vs measured plots to
+        perc (float): colormap range
 
-  fig.savefig(plot_genes_outpath)
+    Returns:
+        N/A
+    """
 
+    genes = gmt_input(genes_filepath)
+    fig = tg.plot_genes_sc(genes, adata_measured=adata_sp, adata_predicted=ad_ge, perc=perc, return_figure=True)
+
+    fig.savefig(plot_genes_outpath)
+
+# TODO: Make parameter assignments more verbose
 def execute_tangram_workflow(args):
     """
     Function to execute the Tangram workflow based on parsed arguments.
@@ -368,25 +438,26 @@ def execute_tangram_workflow(args):
 
     adata_sp = h5ad_input(args.sp)
     adata_sc = h5ad_input(args.sc)
+    celltype_label = args.sc_celltype_field
 
-    plot_initial(adata_sc,adata_sp, args.spatial_alpha, args.umap_point_size, args.spatial_plot_filename, args.umap_plot_filename)
+    plot_initial(adata_sc, adata_sp, celltype_label, args.sp_cluster_field, args.spatial_alpha, args.umap_point_size, args.spatial_plot_filename, args.umap_plot_filename)
 
-    markers = read_markers(adata_sc, args.training_mode, args.number_training_genes, args.marker_genes_input)
+    markers = read_markers(adata_sc, args.training_mode, celltype_label, args.number_training_genes, args.marker_genes_input)
 
     preprocessing(adata_sc, adata_sp, markers)
 
-    adata_map = find_alignment(adata_sc, adata_sp, mode_in=args.alignment_mode, density_in=args.density_prior, num_epochs_in=args.num_epochs, device_in=args.device, cluster_label_in=None)
+    if (args.classification_mode == "Cross-Validation"):
+        ad_ge, df_test_genes = cv(adata_sc, adata_sp, args.alignment_mode, args.cross_val_mode, args.device, args.num_epochs, args.verbose)
+        plot_test_replacement(df_test_genes, args.test_bin_num, args.testing_alpha, args.test_plot_filename)
+    else:
+        adata_map = find_alignment(adata_sc, adata_sp, args.alignment_mode, celltype_label, args.density_prior, args.num_epochs, args.device)
+        map_scrna_to_space(adata_map, adata_sc, adata_sp, celltype_label, args.celltype_plot_filename, args.perc)
+        plot_training_replacement(adata_map, args.training_alpha, args.train_bin_num, args.training_plot_filename)
+        ad_ge = project(adata_map, adata_sc, args.predictions_filename)
 
-    map_scrna_to_space(adata_map, adata_sc, adata_sp, args.annotation_type, args.celltype_plot_filename, args.perc)
-
-    plot_training_replacement(adata_map, args.training_alpha, args.train_bin_num, args.training_plot_filename)
-
-    # TODO: add testing plots and handle the compare_spatial_geneexp() usage to minimize its computation
-
-    ad_ge = project(adata_map, adata_sc, args.predictions_filename)
+        df_all_genes = compare_spatial(ad_ge, adata_sp, adata_sc, args.similarity_scores_filename)
+        plot_auc_replacement(df_all_genes, args.auc_plot_filename)
 
     plot_genes(args.genes_to_plot, adata_sp, ad_ge, args.genes_plot_filename, args.perc)
-
-    plot_auc_replacement(ad_ge, adata_sp, adata_sc, args.auc_plot_filename)
     
     return None
